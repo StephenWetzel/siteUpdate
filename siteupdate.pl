121a52c0a9ce67008a3349a659aef31e1af8604d
Stephen Wetzel <stephen.wetzel1@gmail.com>
2014-05-11 18:48:26 -0400

    change to diff

--- siteupdate.pl
+++ siteupdate.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/perl
 #Site Update by Stephen Wetzel January 11th 2013
-#downloads a site and finds the md5 hash of it
-#if an old md5 hash exsists it compares them and alerts when they've changed
-#it looks in a directory (sites) for any file, each represents a site to check
-#each file has 4 lines: url; start text; end text; md5 hash
-#anything before the start text or after end text is ignored, leave blank to check everything
-
+#checks for updates to websites. 
+#downloads source of web page and extracts an user specified portion. 
+#runs diff on that source every time it's ran to check for updates. 
+#if an update is found it dumps it to stdout. 
+#run with cron, and redirect stdout to email. 
+#
+#put sites in the /sites/ directory. 
+#one text file per site. 
+#line one is the url. 
+#line two is the starting text (can be left blank to start at begining of file). 
+#line three is the ending text (can be left blank). 
+#following lines are ignored. 
 
 
 use strict;
@@ -17,95 +23,81 @@ use Text::Diff;
 
 my $debug = 0; #set to 1 for lots of output
 my $mech = WWW::Mechanize->new();
-my $sitehtml; #stores actual html of site
-my $starttext; #everything on a site before this is ignored
-my $endtext; #everything on a site after this is ignored
-my @filearray; #stores contents of files
-my $fileline; #stores individual lines of a file
+my $siteHtml; #stores actual html of site
+my $startText; #everything on a site before this is ignored
+my $endText; #everything on a site after this is ignored
+my @fileArray; #stores contents of files
+my $thisLine; #stores individual lines of a file
 my $url; #url to check
 my $diff=''; #diff of two versions of site
-my $fname; #file name of the file that has the site info
-my $htmlfile; #file name of html file
-my $startdir = '/home/dale/Documents/Programming/Perl/siteupdate/sites'; #directory that has the files with site infos
-my @filelist; #list of files in the startdir directory
-my $ifile;
-my $ofile;
+my $filename; #file name of the file that has the site info
+my $htmlFile; #file name of html file
+my $startDir = '/home/dale/Documents/Programming/Perl/siteupdate/sites'; #directory that has the files with site infos
+my @fileList; #list of files in the startDir directory
+my $ifile; #Input file
+my $ofile; #output file
 
-opendir(DIR, $startdir) or die "can't open: $startdir \n$!";
-@filelist = readdir(DIR); #a listing of all files/directories including special directories (..)
+opendir(DIR, $startDir) or die "can't open: $startDir \n$!";
+@fileList = readdir(DIR); #a listing of all files/directories including special directories (..)
 
-
-
-foreach $fname (@filelist)
+foreach $filename (@fileList)
 {#each of these files should represent a site to be checked
-	next if ($fname =~ m/^\./); #skip the special directory
-	next if ($fname !~ m/\.txt$/); #only txt files
-	if ($debug) {print "\n$fname";}
-	$fname = $startdir . "/" . $fname; #append directory to filename
-	$htmlfile = $fname;
-	$htmlfile =~ s/\.txt$/\.html/;
+	next if ($filename =~ m/^\./); #skip the special directory
+	next if ($filename !~ m/\.txt$/); #skip non text files
+	if ($debug) {print "\n$filename";}
+	$filename = $startDir . "/" . $filename; #append directory to filename
+	$htmlFile = $filename;
+	$htmlFile =~ s/\.txt$/\.html/; #change extension to html
 	
-	open $ifile, '<', $fname;
-	@filearray = <$ifile>; #load the contents of a config file for this site
+	open $ifile, '<', $filename;
+	@fileArray = <$ifile>; #load the contents of a config file for this site
 	close $ifile;
 	
 	
-	
-	
-	my $oldhtml = do {
-	local $/ = undef;
-	open $ifile, "<", $htmlfile	or 1 or open $ofile, ">", $htmlfile;
-	<$ifile>;
+	my $oldHtml = do 
+	{
+		local $/ = undef; #default record seperator, change to nothing, read entire file
+		open $ifile, "<", $htmlFile	or 1 or open $ofile, ">", $htmlFile;
+		<$ifile>; #dump contents to oldHtml
 	};
 	
-	
-	$url = $filearray[0]; #the url is on the first line
+	$url = $fileArray[0]; #the url is on the first line
 	chomp($url);
 
-	$starttext = $filearray[1]; #starting text is on 2nd line
-	chomp($starttext);
-	$starttext = quotemeta $starttext;
+	$startText = $fileArray[1]; #starting text is on 2nd line
+	chomp($startText);
+	$startText = quotemeta $startText; #ensure anything in startText won't interfere with regex
 
-	$endtext = $filearray[2]; #ending text is on 3rd line
-	chomp($endtext);
-	$endtext = quotemeta $endtext;
+	$endText = $fileArray[2]; #ending text is on 3rd line
+	chomp($endText);
+	$endText = quotemeta $endText;
 
-	if ($debug) {print "\nurl - $url\nstart - $starttext\nend - $endtext\ndiff- $diff";}
+	if ($debug) {print "\nurl - $url\nstart - $startText\nend - $endText\ndiff- $diff";}
 	
 	$mech->add_header( Referer => undef );
 	$mech->get($url); #grab the site data
-	$sitehtml = $mech->res->decoded_content; #convert to html
-	#if ($debug) {print $sitehtml;}
-	$sitehtml = encode_utf8($sitehtml); #get rid of unicode
-	if ($starttext) {$sitehtml =~ s/.*?$starttext//s;} #strip everything prior to start text
-	if ($endtext) {$sitehtml =~ s/$endtext.*//s}; #strip everything after the end text
+	$siteHtml = $mech->res->decoded_content; #convert to html
+	$siteHtml = encode_utf8($siteHtml); #get rid of unicode
+	if ($startText) {$siteHtml =~ s/.*?$startText//s;} #strip everything prior to start text
+	if ($endText) {$siteHtml =~ s/$endText.*//s}; #strip everything after the end text
 	
-	
-	$diff = diff \$oldhtml, \$sitehtml;
-	
-	
+	$diff = diff \$oldHtml, \$siteHtml; #find the diff
 	
 	if ($debug) {print "\ndiff: $diff";}
 	if ($diff)
-	{#site updated
+	{#there is a diff, site updated
 		if ($debug) {print "\n\nUPDATE\n\n";}
 		print "\nUPDATE:";
-		print "\nurl - $url\nstart - $starttext\nend - $endtext\nDIFF:\n$diff";
+		print "\nurl - $url\nstart - $startText\nend - $endText\nDIFF:\n$diff";
 		
-		if ($debug) {print "\nOLD HTML:\n$oldhtml \nNEW HTML:\n$sitehtml";}
+		if ($debug) {print "\nOLD HTML:\n$oldHtml \nNEW HTML:\n$siteHtml";}
 		open $ofile, '>', 'dump.html';
-		print $ofile $sitehtml;
+		print $ofile $siteHtml;
 		close $ofile;
-		#system("leafpad $fname&")
 	}
-	#if ($debug) {print "\n\n\n$sitehtml";}
 	
-	#open my $htmlfile, '>', 'html.html';
-	#print $htmlfile $sitehtml;
-	#close $htmlfile;
-	
-	open $ofile, '>', $htmlfile;
-	print $ofile $sitehtml;
+	open $ofile, '>', $htmlFile;
+	print $ofile $siteHtml;
 	close $ofile;
 
 	if ($debug) {print "\n";}
